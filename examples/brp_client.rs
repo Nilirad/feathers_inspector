2c3c0bea63a7ea351c4356162edeefe6b9c3c12c
Nilirad <gisquerin@gmail.com>
2025-12-08 15:16:36 +0100

    Reduce redundancy in `brp_client` helper module

--- examples/brp_client.rs
+++ examples/brp_client.rs
@@ -11,14 +11,19 @@ use bevy::prelude::*;
 use bevy::remote::BrpRequest;
 use bevy::remote::builtin_methods::{BRP_QUERY_METHOD, BrpQuery, BrpQueryFilter, BrpQueryParams};
 use bevy::remote::http::{DEFAULT_ADDR, DEFAULT_PORT};
-use feathers_inspector::component_inspection::{ComponentDetailLevel, ComponentInspectionSettings};
-use feathers_inspector::entity_inspection::{
-    EntityInspectionSettings, MultipleEntityInspectionSettings,
+use feathers_inspector::brp;
+use feathers_inspector::component_inspection::{
+    ComponentDetailLevel, ComponentInspection, ComponentInspectionSettings, ComponentMetadataMap,
+    ComponentTypeInspection,
 };
-use feathers_inspector::resource_inspection::ResourceInspectionSettings;
-use feathers_inspector::summary::SummarySettings;
+use feathers_inspector::entity_inspection::{
+    EntityInspection, EntityInspectionError, EntityInspectionSettings,
+    MultipleEntityInspectionSettings,
+};
+use feathers_inspector::resource_inspection::{ResourceInspection, ResourceInspectionSettings};
+use feathers_inspector::summary::{SummarySettings, WorldSummary};
 
-use crate::helper::{inspect_component, inspect_multiple};
+use crate::helper::{construct_request, post_request, query};
 
 const SPRITE_COMPONENT_NAME: &str = "bevy_sprite::sprite::Sprite";
 const AMBIENT_LIGHT_COMPONENT_NAME: &str = "bevy_light::ambient_light::AmbientLight";
@@ -89,8 +94,14 @@ fn inspect_all_entities_when_space_pressed(
             filter: BrpQueryFilter::default(),
             strict: false,
         };
-        let entities = helper::query(query_params, &brp_url.0);
-        let component_metadata = helper::generate_component_metadata_map(&brp_url.0);
+        let entities = query(query_params, &brp_url.0);
+        let request = BrpRequest {
+            jsonrpc: String::from("2.0"),
+            method: brp::component_metadata_map_generate::METHOD.to_string(),
+            id: None,
+            params: None,
+        };
+        let metadata_map = post_request::<ComponentMetadataMap>(request, &brp_url.0);
         let settings = MultipleEntityInspectionSettings {
             entity_settings: EntityInspectionSettings {
                 include_components: false,
@@ -98,7 +109,16 @@ fn inspect_all_entities_when_space_pressed(
             },
             ..default()
         };
-        let inspections = inspect_multiple(entities, settings, component_metadata, &brp_url.0);
+        let params = brp::inspect_multiple::Params {
+            entities: entities.into_iter().collect::<Vec<Entity>>(),
+            settings,
+            metadata_map,
+        };
+        let request = construct_request(brp::inspect_multiple::METHOD, params);
+        let inspections = post_request::<Vec<Result<EntityInspection, EntityInspectionError>>>(
+            request, &brp_url.0,
+        );
+
         for result in inspections {
             if let Ok(inspection) = result {
                 info!("{inspection}");
@@ -122,18 +142,20 @@ fn inspect_specific_component_when_c_pressed(
             },
             strict: false,
         };
-        let entities = helper::query(query_params, &brp_url.0);
+        let entities = query(query_params, &brp_url.0);
         let settings = ComponentInspectionSettings {
             detail_level: ComponentDetailLevel::Values,
             full_type_names: true,
         };
         for entity in entities {
-            let inspection = inspect_component(
-                SPRITE_COMPONENT_NAME.to_string(),
+            let params = brp::inspect_component::Params {
+                component_type: SPRITE_COMPONENT_NAME.to_string(),
                 entity,
                 settings,
-                &brp_url.0,
-            );
+                metadata_map: None,
+            };
+            let request = construct_request(brp::inspect_component::METHOD, params);
+            let inspection = post_request::<ComponentInspection>(request, &brp_url.0);
             info!("{inspection}");
         }
     }
@@ -147,11 +169,13 @@ fn inspect_resource_when_r_pressed(
         let settings = ResourceInspectionSettings {
             full_type_names: true,
         };
-        let inspection = helper::inspect_resource(
-            AMBIENT_LIGHT_COMPONENT_NAME.to_string(),
+        let params = brp::inspect_resource::Params {
+            component_type: AMBIENT_LIGHT_COMPONENT_NAME.to_string(),
             settings,
-            &brp_url.0,
-        );
+            metadata_map: None,
+        };
+        let request = construct_request(brp::inspect_resource::METHOD, params);
+        let inspection = post_request::<ResourceInspection>(request, &brp_url.0);
         info!("{inspection}");
     }
 }
@@ -164,7 +188,9 @@ fn inspect_all_resources_when_a_pressed(
         let settings = ResourceInspectionSettings {
             full_type_names: false,
         };
-        let inspections = helper::inspect_all_resources(settings, &brp_url.0);
+        let params = brp::inspect_all_resources::Params { settings };
+        let request = construct_request(brp::inspect_all_resources::METHOD, params);
+        let inspections = post_request::<Vec<ResourceInspection>>(request, &brp_url.0);
         for inspection in inspections {
             info!("{inspection}");
         }
@@ -176,8 +202,12 @@ fn inspect_sprite_component_type_when_m_pressed(
     brp_url: Res<BrpUrl>,
 ) {
     if keyboard_input.just_pressed(KeyCode::KeyM) {
-        let inspection =
-            helper::inspect_component_type(SPRITE_COMPONENT_NAME.to_string(), &brp_url.0);
+        let params = brp::inspect_component_type::Params {
+            component_type: SPRITE_COMPONENT_NAME.to_string(),
+            metadata_map: None,
+        };
+        let request = construct_request(brp::inspect_component_type::METHOD, params);
+        let inspection = post_request::<ComponentTypeInspection>(request, &brp_url.0);
         info!("{inspection}");
     }
 }
@@ -185,7 +215,9 @@ fn inspect_sprite_component_type_when_m_pressed(
 fn summarize_when_s_pressed(keyboard_input: Res<ButtonInput<KeyCode>>, brp_url: Res<BrpUrl>) {
     if keyboard_input.just_pressed(KeyCode::KeyS) {
         let settings = SummarySettings::default();
-        let summary = helper::summarize(settings, &brp_url.0);
+        let params = brp::summarize::Params { settings };
+        let request = construct_request(brp::summarize::METHOD, params);
+        let summary = post_request::<WorldSummary>(request, &brp_url.0);
         info!("{summary}");
     }
 }
@@ -195,17 +227,7 @@ fn summarize_when_s_pressed(keyboard_input: Res<ButtonInput<KeyCode>>, brp_url: 
 // TODO: Helpers should return concrete types instead of a JSON string,
 //       just like `generate_component_metadata` does.
 mod helper {
-    use feathers_inspector::{
-        brp,
-        component_inspection::{
-            ComponentInspection, ComponentMetadataMap, ComponentTypeInspection,
-        },
-        entity_inspection::{
-            EntityInspection, EntityInspectionError, MultipleEntityInspectionSettings,
-        },
-        resource_inspection::{ResourceInspection, ResourceInspectionSettings},
-        summary::{SummarySettings, WorldSummary},
-    };
+    use serde::{Serialize, de::DeserializeOwned};
 
     use super::*;
 
@@ -237,108 +259,25 @@ mod helper {
             .unwrap_or_default()
     }
 
-    #[allow(dead_code)]
-    pub fn inspect_entity(
-        entity: Entity,
-        settings: EntityInspectionSettings,
-        url: &str,
-    ) -> EntityInspection {
-        let brp_request = BrpRequest {
+    pub fn construct_request<T>(method: &str, params: T) -> BrpRequest
+    where
+        T: Serialize,
+    {
+        BrpRequest {
             jsonrpc: String::from("2.0"),
-            method: brp::inspect::METHOD.to_string(),
+            method: method.to_string(),
             id: None,
             params: Some(
-                serde_json::to_value(brp::inspect::Params { entity, settings })
+                serde_json::to_value(params)
                     .expect("Unable to convert query parameters to a valid JSON value"),
             ),
-        };
-        let response = ureq::post(url)
-            .send_json(brp_request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<EntityInspection>(result.clone())
-            .expect("Failed to deserialize `EntityInspection`")
+        }
     }
 
-    #[allow(dead_code)]
-    pub fn inspect_entity_cached(
-        entity: Entity,
-        metadata_map: &ComponentMetadataMap,
-        settings: EntityInspectionSettings,
-        url: &str,
-    ) -> EntityInspection {
-        let brp_request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_cached::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_cached::Params {
-                    entity,
-                    settings,
-                    metadata_map: metadata_map.clone(),
-                })
-                .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(brp_request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<EntityInspection>(result.clone())
-            .expect("Failed to deserialize `EntityInspection`")
-    }
-
-    pub fn inspect_multiple(
-        entities: impl IntoIterator<Item = Entity>,
-        settings: MultipleEntityInspectionSettings,
-        metadata_map: ComponentMetadataMap,
-        url: &str,
-    ) -> Vec<Result<EntityInspection, EntityInspectionError>> {
-        let brp_request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_multiple::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_multiple::Params {
-                    entities: entities.into_iter().collect::<Vec<Entity>>(),
-                    settings,
-                    metadata_map,
-                })
-                .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(brp_request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<Vec<Result<EntityInspection, EntityInspectionError>>>(
-            result.clone(),
-        )
-        .expect("Failed to deserialize `Vec<Result<EntityInspection, EntityInspectionError>>`")
-    }
-
-    pub fn generate_component_metadata_map(url: &str) -> ComponentMetadataMap {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::component_metadata_map_generate::METHOD.to_string(),
-            id: None,
-            params: None,
-        };
+    pub fn post_request<T>(request: BrpRequest, url: &str) -> T
+    where
+        T: DeserializeOwned,
+    {
         let response = ureq::post(url)
             .send_json(request)
             .expect("Failed to send JSON to server")
@@ -348,146 +287,6 @@ mod helper {
         let result = response
             .get("result")
             .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<ComponentMetadataMap>(result.clone())
-            .expect("Failed to deserialize `ComponentMetadataMap`")
-    }
-
-    pub fn inspect_component(
-        component_type: String,
-        entity: Entity,
-        settings: ComponentInspectionSettings,
-        url: &str,
-    ) -> ComponentInspection {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_component::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_component::Params {
-                    component_type,
-                    entity,
-                    settings,
-                    metadata_map: None,
-                })
-                .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<ComponentInspection>(result.clone())
-            .expect("Failed to deserialize `ComponentInspection`")
-    }
-
-    pub fn inspect_resource(
-        component_type: String,
-        settings: ResourceInspectionSettings,
-        url: &str,
-    ) -> ResourceInspection {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_resource::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_resource::Params {
-                    component_type,
-                    settings,
-                    metadata_map: None,
-                })
-                .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<ResourceInspection>(result.clone())
-            .expect("Failed to deserialize `ResourceInspection`")
-    }
-
-    pub fn inspect_all_resources(
-        settings: ResourceInspectionSettings,
-        url: &str,
-    ) -> Vec<ResourceInspection> {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_all_resources::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_all_resources::Params { settings })
-                    .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<Vec<ResourceInspection>>(result.clone())
-            .expect("Failed to deserialize `Vec<ResourceInspection>`")
-    }
-
-    pub fn inspect_component_type(component_type: String, url: &str) -> ComponentTypeInspection {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::inspect_component_type::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::inspect_component_type::Params {
-                    component_type,
-                    metadata_map: None,
-                })
-                .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<ComponentTypeInspection>(result.clone())
-            .expect("Failed to deserialize `ComponentTypeInspection`")
-    }
-
-    pub fn summarize(settings: SummarySettings, url: &str) -> WorldSummary {
-        let request = BrpRequest {
-            jsonrpc: String::from("2.0"),
-            method: brp::summarize::METHOD.to_string(),
-            id: None,
-            params: Some(
-                serde_json::to_value(brp::summarize::Params { settings })
-                    .expect("Unable to convert query parameters to a valid JSON value"),
-            ),
-        };
-        let response = ureq::post(url)
-            .send_json(request)
-            .expect("Failed to send JSON to server")
-            .body_mut()
-            .read_json::<serde_json::Value>()
-            .expect("Failed to read JSON response");
-        let result = response
-            .get("result")
-            .expect("Missing `result` field in JSON-RPC response");
-        serde_json::from_value::<WorldSummary>(result.clone())
-            .expect("Failed to deserialize `WorldSummary`")
+        serde_json::from_value::<T>(result.clone()).expect("Failed to deserialize")
     }
 }
